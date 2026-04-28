ed934f2e8c0df66d4a7353e58a4797816bfffa5e
Gabriel Deva <111201911994@dinus.ac.id>
2026-04-28 09:34:18 +0700

    UTS sisi klien

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Exercise from './Exercise.jsx'
+import PostCard from './PostCard.jsx'
+import Postlist from './PostList.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Exercise />
+    <Postlist />
   </StrictMode>,
 )
